db906922f4d826356610b466ac9a6bd6f006d6f5
Tyler Rust <tylerrust10@gmail.com>
2023-07-22 13:54:02 -0400

    Finished adding Sections, fixed styling

--- src/constants/index.js
+++ src/constants/index.js
@@ -123,6 +123,20 @@ import {
         "Participating in code reviews and providing constructive feedback to other developers.",
       ],
     },
+
+    {
+      title: "Research Assistant",
+      company_name: "Myself",
+      icon: logo,
+      iconBg: "#E6DEDD",
+      date: "Jan 2023 - Present",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
   ];
   
   const testimonials = [
